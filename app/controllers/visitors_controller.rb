7e724b48f29e8065176b5b004b57d455d3cce5ed
Pholisa-Fatyela <pholisa@projectcodex.co>
2016-05-04 14:39:44 +0200

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,11 @@
 class VisitorsController < ApplicationController
   def new
-    
+
     @owner = Owner.new
     render 'visitors/new'
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
+    # flash[:warning] = 'What do you do?'
   end
 
 end
